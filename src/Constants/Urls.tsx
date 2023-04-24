fa9437ea898420cb86a3a3e74fe2f02ec1d023e4
Yamil Diego <yamildiego@gmail.com>
2023-04-24 23:21:59 +1000

    connecting frnt with back

--- src/Constants/Urls.tsx
+++ src/Constants/Urls.tsx
@@ -1,6 +1,4 @@
-// const baseUrl = process.env.REACT_APP_BASE_URL;
-const baseUrl = "http://localhost:3333";
-// const baseUrl = "http://127.0.0.1:3333";
+const baseUrl = process.env.REACT_APP_API_URL;
 
 const Urls = {
   baseUrl,
